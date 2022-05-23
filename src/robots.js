f676ba8fc9cca04ce514363c964e6544ff3a78f1
Mel Panugaling <melpanugaling44@gmail.com>
2022-05-22 19:07:53 -0700

    Uploading for use on other devices

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Hoshimachi Suisei',
+    username: 'Suisimpin',
+    email: 'suiseihosimati@april.biz'
+  },
+  {
+    id: 2,
+    name: 'Watson Amelia',
+    username: 'AmeW',
+    email: 'watsonAme@gmail.com'
+  },
+  {
+    id: 3,
+    name: 'Miko Sakura',
+    username: 'MikoSak',
+    email: 'MikoSaku@gmail.com'
+  },
+  {
+    id: 4,
+    name: 'Tokino Sora',
+    username: 'TSora',
+    email: 'SoraTokino@kory.org'
+  },
+  {
+    id: 5,
+    name: 'Yozora Mel',
+    username: 'YoMel',
+    email: 'Yozomel@annie.ca'
+  },
+  {
+    id: 6,
+    name: 'Natsuiro Matsuri',
+    username: 'Natsumatsu',
+    email: 'NatsuMatsu@jasper.info'
+  },
+  {
+    id: 7,
+    name: 'Minato Aqua',
+    username: 'Mina.Aqua',
+    email: 'Mina.Aqua@billy.biz'
+  },
+  {
+    id: 8,
+    name: 'Shirogane Noel',
+    username: 'ShiroNoe',
+    email: 'ShiroNoe@rosamond.me'
+  },
+  {
+    id: 9,
+    name: 'Omaru Polka',
+    username: 'Omapol',
+    email: 'OmaruPol@dana.io'
+  },
+  {
+    id: 10,
+    name: 'Mori Calliope',
+    username: 'CalliMori',
+    email: 'CalliMori@karina.biz'
+  }
+];
